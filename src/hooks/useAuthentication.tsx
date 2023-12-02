9493940fb5e5d8df4732c1e911c5993cf9bdf40f
root <aslimajr@outlook.com>
2023-12-02 13:56:46 -0300

    Criando o hook useAuthentication

---
+++ src/hooks/useAuthentication.tsx
@@ -0,0 +1,85 @@
+import { app } from '../firebase/config'
+
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	updateProfile,
+} from 'firebase/auth'
+import { useState, useEffect } from 'react'
+import { User } from '../interfaces/user.interface'
+import { FirebaseError } from 'firebase/app'
+
+export const useAuthentication = () => {
+	const [error, setError] = useState('')
+	const [loading, setLoading] = useState(false)
+	// cleanup
+	// deal with memory leaks
+	const [cancelled, setCancelled] = useState(false)
+
+	const auth = getAuth(app)
+
+	function checkIfCancelled() {
+		if (cancelled) {
+			return
+		}
+	}
+
+	const createUser = async (data: User) => {
+		checkIfCancelled()
+
+		setLoading(true)
+		setError('')
+
+		try {
+			const { user } = await createUserWithEmailAndPassword(
+				auth,
+				data.email,
+				data.password
+			)
+
+			await updateProfile(user, {
+				displayName: data.displayName,
+			})
+
+			return user
+		} catch (error: unknown) {
+			if (error instanceof FirebaseError) {
+				console.log(error.message)
+				console.log(typeof error.message)
+
+				let systemErrorMessage
+
+				switch (error.code) {
+					case 'auth/email-already-in-use':
+						systemErrorMessage = 'Email já está em uso'
+						break
+					case 'auth/invalid-email':
+						systemErrorMessage = 'Email inválido'
+						break
+					case 'auth/weak-password':
+						systemErrorMessage = 'Senha fraca'
+						break
+					default:
+						systemErrorMessage = 'Erro desconhecido'
+				}
+
+				setError(systemErrorMessage)
+			}
+		} finally {
+			setLoading(false)
+		}
+	}
+
+	useEffect(() => {
+		return () => {
+			setCancelled(true)
+		}
+	}, [])
+
+	return {
+		auth,
+		createUser,
+		error,
+		loading,
+	}
+}
