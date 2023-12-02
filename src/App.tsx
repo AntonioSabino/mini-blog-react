688f2d05a5293ef02695da663b425a8949b91e96
root <aslimajr@outlook.com>
2023-12-02 11:10:59 -0300

    Adicionando o Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Header />
 			<div className='container'>
 				<Routes>
 					<Route
@@ -22,6 +25,7 @@ function App() {
 					/>
 				</Routes>
 			</div>
+			<Footer />
 		</BrowserRouter>
 	)
 }
