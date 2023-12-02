80931163d9b8c0c395c5e0c902b8d48f5afd6b32
root <aslimajr@outlook.com>
2023-12-02 01:23:12 -0300

    Configurando o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
 	return (
-		<>
-			<h1>Mini Blog</h1>
-		</>
+		<BrowserRouter>
+			<div className='container'>
+				<Routes>
+					<Route
+						path='/'
+						element={<Home />}
+					/>
+					<Route
+						path='about'
+						element={<About />}
+					/>
+					<Route
+						path='*'
+						element={<h1>Not Found</h1>}
+					/>
+				</Routes>
+			</div>
+		</BrowserRouter>
 	)
 }
 
