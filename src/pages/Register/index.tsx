6470c21b84209c21f33c334fd0dea4137af2d1f6
root <aslimajr@outlook.com>
2023-12-02 13:14:54 -0300

    Adicionando o state form da página Register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,37 @@
+import { useState } from 'react'
 import styles from './Register.module.css'
 
 const Register = () => {
+	const [displayName, setDisplayName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [confirmPassword, setConfirmPassword] = useState('')
+	const [error, setError] = useState('')
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		setError('')
+
+		const user = {
+			displayName,
+			email,
+			password,
+		}
+
+		if (password !== confirmPassword) {
+			setError('As senhas senhas precisam ser iguais')
+			return
+		}
+
+		console.log(user)
+	}
+
 	return (
 		<div className={styles.register}>
 			<h1>Cadastre-se para postar</h1>
 			<p>Crie seu usuário e compartilhe as suas histórias</p>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label>
 					<span>Nome:</span>
 					<input
@@ -13,6 +39,8 @@ const Register = () => {
 						name='displayName'
 						required
 						placeholder='Nome do usuário'
+						value={displayName}
+						onChange={(e) => setDisplayName(e.target.value)}
 					/>
 				</label>
 				<label>
@@ -22,6 +50,8 @@ const Register = () => {
 						name='email'
 						required
 						placeholder='Email do usuário'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
 					/>
 				</label>
 				<label>
@@ -31,6 +61,8 @@ const Register = () => {
 						name='password'
 						required
 						placeholder='Senha do usuário'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
 					/>
 				</label>
 				<label>
@@ -40,14 +72,17 @@ const Register = () => {
 						name='confirmPassword'
 						required
 						placeholder='Confirme a senha do usuário'
+						value={confirmPassword}
+						onChange={(e) => setConfirmPassword(e.target.value)}
 					/>
 				</label>
 				<button
-					type='button'
+					type='submit'
 					className='btn'
 				>
 					Cadastrar
 				</button>
+				{error && <p className='error'>{error}</p>}
 			</form>
 		</div>
 	)
