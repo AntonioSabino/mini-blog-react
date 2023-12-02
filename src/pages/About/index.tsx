1d93b41e4bc1ddf7ec34283142f6ede9bf8012a3
root <aslimajr@outlook.com>
2023-12-02 20:28:11 -0300

    Criando estrutura da página About

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,22 @@
-// import styles from './About.module.css'
+import styles from './About.module.css'
+
+import { Link } from 'react-router-dom'
 
 const About = () => {
-	return <h1>About</h1>
+	return (
+		<div className={styles.about}>
+			<h2>
+				Sobre o Mini <span>Blog</span>
+			</h2>
+			<p>Este projeto consiste me um blog feito com ReactJS e Firebase.</p>
+			<Link
+				to='/posts/create'
+				className='btn'
+			>
+				Criar Post
+			</Link>
+		</div>
+	)
 }
 
 export default About
