8b6c9d4d00f1d25782cd890c042dc4d7d4cff2e1
root <aslimajr@outlook.com>
2023-12-02 01:12:51 -0300

    Configurando o firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBHFQPAGBgBq02ubJmrVV11ugBum9rGgyk',
+	authDomain: 'mini-blog-91e39.firebaseapp.com',
+	projectId: 'mini-blog-91e39',
+	storageBucket: 'mini-blog-91e39.appspot.com',
+	messagingSenderId: '228619554385',
+	appId: '1:228619554385:web:b7c01c6c965d5896583270',
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+export default db
