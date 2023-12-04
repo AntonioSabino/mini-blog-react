1b0ce81630f7f2151dcc71057f22dcc6fb42abf1
root <aslimajr@outlook.com>
2023-12-04 17:31:42 -0300

    Exibindo resultado da pesquisa

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,13 +1,43 @@
+import styles from './Search.module.css'
+
+import { Link } from 'react-router-dom'
+import PostDetail from '../../components/PostDetail'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 import { useQuery } from '../../hooks/useQuery'
 
 const Search = () => {
 	const query = useQuery()
-	const search = query.get('q')
+	const search: string = query.get('q') || ''
+
+	const { documents: posts } = useFetchDocuments('posts', search)
 
 	return (
-		<div>
+		<div className={styles.search_container}>
 			<h2>Search</h2>
-			<p>{search}</p>
+			<div>
+				{posts && posts.length === 0 && (
+					<>
+						<p>Não foram encontrados posts</p>
+						<Link
+							to='/'
+							className='btn btn-dark'
+						>
+							Voltar
+						</Link>
+					</>
+				)}
+				{posts &&
+					posts.map((post) => (
+						<PostDetail
+							key={post.id}
+							id={post.id}
+							title={post.title}
+							image={post.image}
+							createdBy={post.createdBy}
+							tags={post.tags}
+						/>
+					))}
+			</div>
 		</div>
 	)
 }
