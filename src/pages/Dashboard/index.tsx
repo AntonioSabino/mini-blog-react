1b4e409dc18848691655d37c2e3608d2277fc5f2
root <aslimajr@outlook.com>
2023-12-11 21:21:42 -0300

    estruturando a pagina Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,31 @@
+import styles from './Dashboard.module.css'
+
+import { useAuthValue } from '../../hooks/useAuthValue'
+import { Link } from 'react-router-dom'
+
 const Dashboard = () => {
+	const user = useAuthValue()
+	const uid = user.uid
+
+	const posts = []
+
 	return (
 		<div>
-			<h1>Dashboard</h1>
+			<h2>Dashboard</h2>
+			<p>Gerencie os seus posts</p>
+			{posts && posts.length === 0 ? (
+				<div className={styles.noposts}>
+					<p>Não foram encontrados posts</p>
+					<Link
+						to='/posts/create'
+						className='btn'
+					>
+						Criar primeiro post
+					</Link>
+				</div>
+			) : (
+				<p>Posts</p>
+			)}
 		</div>
 	)
 }
